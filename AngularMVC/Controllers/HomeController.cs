7f8251c95341472cf53ed1a745539b7d30dadcf6
Waqas <Waqas>
2017-05-28 16:41:09 +1000

    Add project files.

---
+++ AngularMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using AngularMVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult savePerson(Person person)
+        {
+            person.ID = 1;
+            return Json(person);
+        }
+
+    }
+}
---
+++ AngularMVC/ng-Script/HomeModule.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    var app = angular.module("homeApp", []);
+    var controller = function ($scope, $http, $filter) {
+        $scope.selectedObject = {};
+        //$scope.changeNameTo = function () {
+
+        //}
+        //var myInternalFunc = function() {
+
+        //}
+        var savePersonError = function (reason) {
+
+        }
+        var savePersonSuccess = function (response) {
+            $scope.selectedObject = response.data;
+        }
+        $scope.savePerson = function () {
+            $http.post('/Home/savePerson', $scope.selectedObject).then(savePersonSuccess, savePersonError)
+        }
+        $scope.selectPerson = function (objP) {
+            $scope.selectedObject = objP;
+        }
+        $scope.persons = [
+            { 'FirstName': 'John', 'LastName': 'Doe' },
+            { 'FirstName': 'Jane', 'LastName': 'Doe' },
+            { 'FirstName': 'David', 'LastName': 'Smith' }
+        ];
+    }
+    app.controller('homeCtrl', controller);
+})();
